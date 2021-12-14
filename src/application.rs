75637c52dd883ec47226203813f41593c8fc8913
om <oliver.malham@gmail.com>
2021-12-14 21:13:02 +0000

    Managed to get the refacored code to compile and run on the pico

--- src/application.rs
+++ src/application.rs
@@ -1,23 +1,35 @@
-use crate::state::ApplicationState;
-use defmt::*;
+// use defmt::*;
+use defmt_rtt as _;
 
-// Configure application specific state, pins etc.
-// Run after the initial basic setup
-pub fn setup(state: &ApplicationState) {
-    // let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
-    // let mut led_pin = pins.led.into_push_pull_output();
-}
+// use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::PwmPin;
+// use pico::hal::gpio::{bank0::Gpio25, Pin, PushPullOutput};
+use pico::hal::pwm::{Channel, FreeRunning, Pwm4, B};
 
-// Application main loop
-pub fn application_loop(state: &ApplicationState) {
-    let mut delay =
-        cortex_m::delay::Delay::new(state.core.SYST, clocks.system_clock.freq().integer());
-    let mut led_pin = pins.led.into_push_pull_output();
+// The minimum PWM value (i.e. LED brightness) we want
+const LOW: u16 = 0;
 
-    info!("on!");
-    led_pin.set_high().unwrap();
-    delay.delay_ms(500);
-    info!("off!");
-    led_pin.set_low().unwrap();
-    delay.delay_ms(500);
+// The maximum PWM value (i.e. LED brightness) we want
+const HIGH: u16 = 25000;
+
+pub fn application_main(
+    //mut led_pin: Pin<Gpio25, PushPullOutput>,
+    pwm_channel: &mut Channel<Pwm4, FreeRunning, B>,
+    mut delay: cortex_m::delay::Delay,
+) -> ! {
+    loop {
+        // Ramp brightness up
+        for i in (LOW..=HIGH).skip(100) {
+            delay.delay_us(8);
+            pwm_channel.set_duty(i);
+        }
+
+        // Ramp brightness down
+        for i in (LOW..=HIGH).rev().skip(100) {
+            delay.delay_us(8);
+            pwm_channel.set_duty(i);
+        }
+
+        delay.delay_ms(500);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,83 @@
+//! Blinks the LED on a Pico board
+//!
+//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
 #![no_std]
 #![no_main]
 
 use cortex_m_rt::entry;
 use defmt::*;
 use defmt_rtt as _;
+//use embedded_hal::digital::v2::OutputPin;
+use embedded_time::fixed_point::FixedPoint;
 use panic_probe as _;
 
 mod application;
-mod state;
+use crate::application::application_main;
 
-use crate::state::ApplicationState;
+// use pico::hal::{
+//     clocks::{init_clocks_and_plls, Clock},
+//     gpio::Pin,
+//     gpio::PushPullOutput,
+//     pac,
+//     sio::Sio,
+//     watchdog::Watchdog,
+// };
+// use pico::Pins;
+
+use pico::hal::{
+    clocks::{init_clocks_and_plls, Clock},
+    //    gpio::{bank0::Gpio25, Pin, PushPullOutput},
+    pac,
+    sio::Sio,
+    watchdog::Watchdog,
+};
 
 #[entry]
 fn main() -> ! {
     info!("Program start");
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+    let sio = Sio::new(pac.SIO);
 
-    let state = ApplicationState::new();
+    // External high-speed crystal on the pico board is 12Mhz
+    let external_xtal_freq_hz = 12_000_000u32;
+    let clocks = init_clocks_and_plls(
+        external_xtal_freq_hz,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
 
-    // Mimic Ardunio code structure to try and keep application code clear of boiler plate
-    application::setup(&state);
+    let delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
 
-    loop {
-        application::application_loop(&state);
-    }
+    let pins = pico::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    //let led_pin: Pin<Gpio25, PushPullOutput> = pins.led.into_mode();
+
+    // Init PWMs
+    let mut pwm_slices = pico::hal::pwm::Slices::new(pac.PWM, &mut pac.RESETS);
+
+    // Configure PWM4
+    let pwm = &mut pwm_slices.pwm4;
+    pwm.set_ph_correct();
+    pwm.enable();
+
+    // Output channel B on PWM4 to the LED pin
+    let channel = &mut pwm.channel_b;
+    channel.output_to(pins.led);
+
+    application_main(channel, delay)
 }
 
 // End of file
